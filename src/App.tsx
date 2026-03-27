3a61acf1527e1e562f73e4b7895b6d02933ba3ab
ncattin <164329252+ncattin@users.noreply.github.com>
2026-03-27 22:23:32 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Index from "./pages/Index";
+import Missions from "./pages/Missions";
+import Competences from "./pages/Competences";
+import Parcours from "./pages/Parcours";
+import CompetenceDetail from "./pages/CompetenceDetail";
+import Contact from "./pages/Contact";
+import MissionDetail from "./pages/MissionDetail";
+import ProjectDetail from "./pages/ProjectDetail";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/missions" element={<Missions />} />
+            <Route path="/missions/:slug" element={<MissionDetail />} />
+            <Route path="/competences" element={<Competences />} />
+            <Route path="/competences/:slug" element={<CompetenceDetail />} />
+            <Route path="/parcours" element={<Parcours />} />
+            <Route path="/projets/:slug" element={<ProjectDetail />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
